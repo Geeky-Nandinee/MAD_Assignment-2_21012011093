5c1222c60073cb0c90e6238dfc2235ea09481b0a
Nandinee <nandineepatel2003@gmail.com>
2023-10-25 18:02:47 +0530

    Assignment-2

---
+++ app/src/main/java/com/example/mad_assignment_2_21012011093/TotalExpenditureActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mad_assignment_2_21012011093
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TotalExpenditureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_total_expenditure)
+    }
+}
