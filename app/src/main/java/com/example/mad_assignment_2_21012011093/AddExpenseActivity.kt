883d756165d4dca23cb2e16a92ae609f6c5c1595
Nandinee <nandineepatel2003@gmail.com>
2023-10-29 17:54:44 +0530

    Assignment-2

--- app/src/main/java/com/example/mad_assignment_2_21012011093/AddExpenseActivity.kt
+++ app/src/main/java/com/example/mad_assignment_2_21012011093/AddExpenseActivity.kt
@@ -1,24 +1,20 @@
 package com.example.mad_assignment_2_21012011093
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.text.SimpleDateFormat
-import android.app.DatePickerDialog
-import android.content.Intent
-import android.media.MediaPlayer
+import org.json.JSONArray
 import org.json.JSONObject
-import java.util.*
 
 class AddExpenseActivity : AppCompatActivity() {
     private lateinit var editTextAmount: EditText
-    private lateinit var editTextDate: EditText
+    private lateinit var datePicker: DatePicker
     private lateinit var spinnerCategory: Spinner
     private lateinit var editTextDescription: EditText
-    private lateinit var fabAddExpense: FloatingActionButton
+    private lateinit var fabAddExpense: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,79 +22,345 @@ class AddExpenseActivity : AppCompatActivity() {
 
         // Initialize UI elements
         editTextAmount = findViewById(R.id.editTextAmount)
-        editTextDate = findViewById(R.id.editTextDate)
+        datePicker = findViewById(R.id.datePicker)
         spinnerCategory = findViewById(R.id.spinnerCategory)
         editTextDescription = findViewById(R.id.editTextDescription)
         fabAddExpense = findViewById(R.id.fabAddExpense)
 
-        // Set up a click listener for the date input
-        editTextDate.setOnClickListener {
-            showDatePickerDialog()
-        }
-
-        // Set up onClickListener for the Add Expense button
+        // Set up a click listener for the "Add Expense" button
         fabAddExpense.setOnClickListener {
             // Get user input
             val amount = editTextAmount.text.toString()
-            val date = editTextDate.text.toString()
+            val day = datePicker.dayOfMonth
+            val month = datePicker.month + 1 // Months are 0-based, so add 1
+            val year = datePicker.year
+            val date = "$year-$month-$day"
             val category = spinnerCategory.selectedItem.toString()
             val description = editTextDescription.text.toString()
 
-            // Convert the expense data to JSON
-            val expenseJson = convertExpenseToJson(amount, date, category, description)
+            // Create a JSON object for the new expense
+            val expense = JSONObject()
+            expense.put("amount", amount)
+            expense.put("date", date)
+            expense.put("category", category)
+            expense.put("description", description)
 
-            // Implement code to save the expense to SharedPreferences as JSON
-            saveExpenseToSharedPreferences(expenseJson)
+            // Store the new expense in SharedPreferences
+            saveExpense(expense)
 
-            // Add sound feedback (replace with your sound implementation)
-            playExpenseConfirmationSound()
-
-            // Optionally, navigate back to the main screen or perform other actions
+            // Navigate to TotalExpenditureActivity
+            navigateToTotalExpenditure()
         }
+
+        // Initialize the spinner with categories
+        val categories = resources.getStringArray(R.array.expense_categories)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinnerCategory.adapter = adapter
     }
 
-    private fun showDatePickerDialog() {
-        val calendar = Calendar.getInstance()
-        val year = calendar.get(Calendar.YEAR)
-        val month = calendar.get(Calendar.MONTH)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-
-        val datePickerDialog = DatePickerDialog(this, { _, year, month, dayOfMonth ->
-            val selectedDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
-                .format(GregorianCalendar(year, month, dayOfMonth).time)
-            editTextDate.setText(selectedDate)
-        }, year, month, day)
-
-        datePickerDialog.show()
+    private fun navigateToTotalExpenditure() {
+        val intent = Intent(this, TotalExpenditureActivity::class.java)
+        startActivity(intent)
     }
 
-    private fun convertExpenseToJson(
-        amount: String,
-        date: String,
-        category: String,
-        description: String
-    ): String {
-        val jsonObject = JSONObject()
-        jsonObject.put("amount", amount)
-        jsonObject.put("date", date)
-        jsonObject.put("category", category)
-        jsonObject.put("description", description)
-        return jsonObject.toString()
-    }
-
-    private fun saveExpenseToSharedPreferences(expenseJson: String) {
+    private fun saveExpense(expense: JSONObject) {
         val sharedPreferences: SharedPreferences = getSharedPreferences("ExpenseTracker", Context.MODE_PRIVATE)
         val editor = sharedPreferences.edit()
 
-        // Save the JSON data to SharedPreferences
-        editor.putString("expense_data", expenseJson)
+        // Retrieve the existing expenses
+        val expensesArray = JSONArray(sharedPreferences.getString("expenses", "[]"))
+
+        // Add the new expense to the array
+        expensesArray.put(expense)
+
+        // Save the updated array back to SharedPreferences
+        editor.putString("expenses", expensesArray.toString())
         editor.apply()
     }
-
-    private fun playExpenseConfirmationSound() {
-        val serviceIntent = Intent(this, ConfirmationSoundService::class.java)
-        startService(serviceIntent)
-
-    }
-
 }
+
+
+
+
+//package com.example.mad_assignment_2_21012011093
+//
+//import android.app.DatePickerDialog
+//import android.content.Context
+//import android.content.SharedPreferences
+//import android.os.Bundle
+//import android.widget.*
+//import androidx.appcompat.app.AppCompatActivity
+//import org.json.JSONArray
+//import org.json.JSONObject
+//import java.util.Calendar
+//
+//class AddExpenseActivity : AppCompatActivity() {
+//    private lateinit var editTextAmount: EditText
+//    private lateinit var editTextDate: EditText
+//    private lateinit var spinnerCategory: Spinner
+//    private lateinit var editTextDescription: EditText
+//    private lateinit var fabAddExpense: Button
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_add_expense)
+//
+//        // Initialize UI elements
+//        editTextAmount = findViewById(R.id.editTextAmount)
+//        editTextDate = findViewById(R.id.editTextDate)
+//        spinnerCategory = findViewById(R.id.spinnerCategory)
+//        editTextDescription = findViewById(R.id.editTextDescription)
+//        fabAddExpense = findViewById(R.id.fabAddExpense)
+//
+//        // Set up a click listener for the "Add Expense" button
+//        fabAddExpense.setOnClickListener {
+//            // Get user input
+//            val amount = editTextAmount.text.toString()
+//            val date = editTextDate.text.toString()
+//            val category = spinnerCategory.selectedItem.toString()
+//            val description = editTextDescription.text.toString()
+//
+//            // Create a JSON object for the new expense
+//            val expense = JSONObject()
+//            expense.put("amount", amount)
+//            expense.put("date", date)
+//            expense.put("category", category)
+//            expense.put("description", description)
+//
+//            // Store the new expense in SharedPreferences
+//            saveExpense(expense)
+//
+//            // Optionally, navigate back to the main screen or perform other actions
+//        }
+//
+//        // Initialize the spinner with categories
+//        val categories = resources.getStringArray(R.array.expense_categories)
+//        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        spinnerCategory.adapter = adapter
+//
+//        // Set up date picker dialog
+//        editTextDate.setOnClickListener {
+//            showDatePickerDialog()
+//        }
+//    }
+//
+//    private fun showDatePickerDialog() {
+//        val calendar = Calendar.getInstance()
+//        val year = calendar.get(Calendar.YEAR)
+//        val month = calendar.get(Calendar.MONTH)
+//        val day = calendar.get(Calendar.DAY_OF_MONTH)
+//
+//        val datePicker = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+//            // Update the EditText with the selected date
+//            val selectedDate = "${dayOfMonth}/${month + 1}/${year}" // Month is 0-based
+//            editTextDate.setText(selectedDate)
+//        }, year, month, day)
+//
+//        datePicker.show()
+//    }
+//
+//    private fun saveExpense(expense: JSONObject) {
+//        val sharedPreferences: SharedPreferences = getSharedPreferences("ExpenseTracker", Context.MODE_PRIVATE)
+//        val editor = sharedPreferences.edit()
+//
+//        // Retrieve the existing expenses
+//        val expensesArray = JSONArray(sharedPreferences.getString("expenses", "[]"))
+//
+//        // Add the new expense to the array
+//        expensesArray.put(expense)
+//
+//        // Save the updated array back to SharedPreferences
+//        editor.putString("expenses", expensesArray.toString())
+//        editor.apply()
+//    }
+//}
+
+
+
+
+
+//package com.example.mad_assignment_2_21012011093
+//
+//import android.content.Context
+//import android.content.SharedPreferences
+//import android.os.Bundle
+//import android.widget.*
+//import androidx.appcompat.app.AppCompatActivity
+//import org.json.JSONArray
+//import org.json.JSONObject
+//
+//class AddExpenseActivity : AppCompatActivity() {
+//    private lateinit var editTextAmount: EditText
+//    private lateinit var editTextDate: EditText
+//    private lateinit var spinnerCategory: Spinner
+//    private lateinit var editTextDescription: EditText
+//    private lateinit var fabAddExpense: Button
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_add_expense)
+//
+//        // Initialize UI elements
+//        editTextAmount = findViewById(R.id.editTextAmount)
+//        editTextDate = findViewById(R.id.editTextDate)
+//        spinnerCategory = findViewById(R.id.spinnerCategory)
+//        editTextDescription = findViewById(R.id.editTextDescription)
+//        fabAddExpense = findViewById(R.id.fabAddExpense)
+//
+//        // Set up a click listener for the "Add Expense" button
+//        fabAddExpense.setOnClickListener {
+//            // Get user input
+//            val amount = editTextAmount.text.toString()
+//            val date = editTextDate.text.toString()
+//            val category = spinnerCategory.selectedItem.toString()
+//            val description = editTextDescription.text.toString()
+//
+//            // Create a JSON object for the new expense
+//            val expense = JSONObject()
+//            expense.put("amount", amount)
+//            expense.put("date", date)
+//            expense.put("category", category)
+//            expense.put("description", description)
+//
+//            // Store the new expense in SharedPreferences
+//            saveExpense(expense)
+//
+//            // Optionally, navigate back to the main screen or perform other actions
+//        }
+//
+//        // Initialize the spinner with categories
+//        val categories = resources.getStringArray(R.array.expense_categories)
+//        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, categories)
+//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+//        spinnerCategory.adapter = adapter
+//    }
+//
+//    private fun saveExpense(expense: JSONObject) {
+//        val sharedPreferences: SharedPreferences = getSharedPreferences("ExpenseTracker", Context.MODE_PRIVATE)
+//        val editor = sharedPreferences.edit()
+//
+//        // Retrieve the existing expenses
+//        val expensesArray = JSONArray(sharedPreferences.getString("expenses", "[]"))
+//
+//        // Add the new expense to the array
+//        expensesArray.put(expense)
+//
+//        // Save the updated array back to SharedPreferences
+//        editor.putString("expenses", expensesArray.toString())
+//        editor.apply()
+//    }
+//}
+//
+//
+//
+//
+//
+//
+////package com.example.mad_assignment_2_21012011093
+////
+////import android.content.Context
+////import android.content.SharedPreferences
+////import android.os.Bundle
+////import android.widget.*
+////import androidx.appcompat.app.AppCompatActivity
+////import com.google.android.material.floatingactionbutton.FloatingActionButton
+////import java.text.SimpleDateFormat
+////import android.app.DatePickerDialog
+////import android.content.Intent
+////import android.media.MediaPlayer
+////import org.json.JSONObject
+////import java.util.*
+////
+////class AddExpenseActivity : AppCompatActivity() {
+////    private lateinit var editTextAmount: EditText
+////    private lateinit var editTextDate: EditText
+////    private lateinit var spinnerCategory: Spinner
+////    private lateinit var editTextDescription: EditText
+////    private lateinit var fabAddExpense: FloatingActionButton
+////
+////    override fun onCreate(savedInstanceState: Bundle?) {
+////        super.onCreate(savedInstanceState)
+////        setContentView(R.layout.activity_add_expense)
+////
+////        // Initialize UI elements
+////        editTextAmount = findViewById(R.id.editTextAmount)
+////        editTextDate = findViewById(R.id.editTextDate)
+////        spinnerCategory = findViewById(R.id.spinnerCategory)
+////        editTextDescription = findViewById(R.id.editTextDescription)
+////        fabAddExpense = findViewById(R.id.fabAddExpense)
+////
+////        // Set up a click listener for the date input
+////        editTextDate.setOnClickListener {
+////            showDatePickerDialog()
+////        }
+////
+////        // Set up onClickListener for the Add Expense button
+////        fabAddExpense.setOnClickListener {
+////            // Get user input
+////            val amount = editTextAmount.text.toString()
+////            val date = editTextDate.text.toString()
+////            val category = spinnerCategory.selectedItem.toString()
+////            val description = editTextDescription.text.toString()
+////
+////            // Convert the expense data to JSON
+////            val expenseJson = convertExpenseToJson(amount, date, category, description)
+////
+////            // Implement code to save the expense to SharedPreferences as JSON
+////            saveExpenseToSharedPreferences(expenseJson)
+////
+////            // Add sound feedback (replace with your sound implementation)
+////            playExpenseConfirmationSound()
+////
+////            // Optionally, navigate back to the main screen or perform other actions
+////        }
+////    }
+////
+////    private fun showDatePickerDialog() {
+////        val calendar = Calendar.getInstance()
+////        val year = calendar.get(Calendar.YEAR)
+////        val month = calendar.get(Calendar.MONTH)
+////        val day = calendar.get(Calendar.DAY_OF_MONTH)
+////
+////        val datePickerDialog = DatePickerDialog(this, { _, year, month, dayOfMonth ->
+////            val selectedDate = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+////                .format(GregorianCalendar(year, month, dayOfMonth).time)
+////            editTextDate.setText(selectedDate)
+////        }, year, month, day)
+////
+////        datePickerDialog.show()
+////    }
+////
+////    private fun convertExpenseToJson(
+////        amount: String,
+////        date: String,
+////        category: String,
+////        description: String
+////    ): String {
+////        val jsonObject = JSONObject()
+////        jsonObject.put("amount", amount)
+////        jsonObject.put("date", date)
+////        jsonObject.put("category", category)
+////        jsonObject.put("description", description)
+////        return jsonObject.toString()
+////    }
+////
+////    private fun saveExpenseToSharedPreferences(expenseJson: String) {
+////        val sharedPreferences: SharedPreferences = getSharedPreferences("ExpenseTracker", Context.MODE_PRIVATE)
+////        val editor = sharedPreferences.edit()
+////
+////        // Save the JSON data to SharedPreferences
+////        editor.putString("expense_data", expenseJson)
+////        editor.apply()
+////    }
+////
+////    private fun playExpenseConfirmationSound() {
+////        val serviceIntent = Intent(this, ConfirmationSoundService::class.java)
+////        startService(serviceIntent)
+////
+////    }
+////
+////}
+////
+////
--- app/src/main/java/com/example/mad_assignment_2_21012011093/TotalExpenditureActivity.kt
+++ app/src/main/java/com/example/mad_assignment_2_21012011093/TotalExpenditureActivity.kt
@@ -5,13 +5,10 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import org.json.JSONObject
+import org.json.JSONArray
 
 class TotalExpenditureActivity : AppCompatActivity() {
     private lateinit var radioGroupTimeFrame: RadioGroup
-    private lateinit var radioButtonDaily: RadioButton
-    private lateinit var radioButtonWeekly: RadioButton
-    private lateinit var radioButtonMonthly: RadioButton
     private lateinit var textViewTotalExpenditure: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,71 +17,139 @@ class TotalExpenditureActivity : AppCompatActivity() {
 
         // Initialize UI elements
         radioGroupTimeFrame = findViewById(R.id.radioGroupTimeFrame)
-        radioButtonDaily = findViewById(R.id.radioButtonDaily)
-        radioButtonWeekly = findViewById(R.id.radioButtonWeekly)
-        radioButtonMonthly = findViewById(R.id.radioButtonMonthly)
         textViewTotalExpenditure = findViewById(R.id.textViewTotalExpenditure)
 
         // Set up radio button listeners
-        radioGroupTimeFrame.setOnCheckedChangeListener { group, checkedId ->
+        radioGroupTimeFrame.setOnCheckedChangeListener { _, checkedId ->
             when (checkedId) {
-                R.id.radioButtonDaily -> {
-                    // Calculate and display total daily expenditure
-                    val totalDailyExpenditure = calculateTotalExpenditureFor("daily")
-                    textViewTotalExpenditure.text = "Total Expenditure (Daily): $totalDailyExpenditure"
-                }
-                R.id.radioButtonWeekly -> {
-                    // Calculate and display total weekly expenditure
-                    val totalWeeklyExpenditure = calculateTotalExpenditureFor("weekly")
-                    textViewTotalExpenditure.text = "Total Expenditure (Weekly): $totalWeeklyExpenditure"
-                }
-                R.id.radioButtonMonthly -> {
-                    // Calculate and display total monthly expenditure
-                    val totalMonthlyExpenditure = calculateTotalExpenditureFor("monthly")
-                    textViewTotalExpenditure.text = "Total Expenditure (Monthly): $totalMonthlyExpenditure"
-                }
+                R.id.radioButtonDaily -> displayTotalExpenditure("daily")
+                R.id.radioButtonWeekly -> displayTotalExpenditure("weekly")
+                R.id.radioButtonMonthly -> displayTotalExpenditure("monthly")
             }
         }
-
-        // Load and display the JSON expense data
-        val expenseJson = loadExpenseFromSharedPreferences()
-        displayExpenseData(expenseJson)
     }
 
-    private fun calculateTotalExpenditureFor(timeFrame: String): Double {
+    private fun displayTotalExpenditure(timeFrame: String) {
+        val sharedPreferences: SharedPreferences = getSharedPreferences("ExpenseTracker", Context.MODE_PRIVATE)
+
+        // Retrieve the JSON array of expenses
+        val expensesArray = JSONArray(sharedPreferences.getString("expenses", "[]"))
+
+        // Calculate and display the total expenditure based on the selected time frame
+        val totalExpenditure = calculateTotalExpenditure(expensesArray, timeFrame)
+        textViewTotalExpenditure.text = "Total Expenditure ($timeFrame): $totalExpenditure"
+    }
+
+    private fun calculateTotalExpenditure(expensesArray: JSONArray, timeFrame: String): Double {
         // Placeholder for calculating the total expenditure based on the selected time frame
-        // You should replace this with your logic to calculate the actual total expenditure
-        // Sample: Calculate a random total expenditure
-        return when (timeFrame) {
-            "daily" -> 75.0
-            "weekly" -> 450.0
-            "monthly" -> 1900.0
-            else -> 0.0
+        // Replace this with your logic to calculate the actual total expenditure
+        // For simplicity, we'll sum all expenses in this example
+        var total = 0.0
+
+        for (i in 0 until expensesArray.length()) {
+            val expense = expensesArray.getJSONObject(i)
+            // Retrieve the date from the JSON expense object and compare it with the selected time frame
+            val date = expense.getString("date")
+            // Add logic to filter expenses based on the time frame
+            // For demonstration, we assume all expenses are considered for the total
+            total += expense.getDouble("amount")
         }
-    }
 
-    private fun loadExpenseFromSharedPreferences(): String? {
-        val sharedPreferences: SharedPreferences =
-            getSharedPreferences("ExpenseTracker", Context.MODE_PRIVATE)
-
-        // Load the JSON expense data from SharedPreferences
-        return sharedPreferences.getString("expense_data", null)
-    }
-
-    private fun displayExpenseData(expenseJson: String?) {
-        if (expenseJson != null) {
-            // Parse the JSON and extract data for display
-            val jsonObject = JSONObject(expenseJson)
-            val amount = jsonObject.getString("amount")
-            val date = jsonObject.getString("date")
-            val category = jsonObject.getString("category")
-            val description = jsonObject.getString("description")
-
-            // Display the data in a TextView or other UI elements
-            textViewTotalExpenditure.text = "Amount: $amount\nDate: $date\nCategory: $category\nDescription: $description"
-        } else {
-            // Handle the case where there is no expense data to display
-            textViewTotalExpenditure.text = "No expense data available"
-        }
+        return total
     }
 }
+
+
+
+
+//package com.example.mad_assignment_2_21012011093
+//
+//import android.content.Context
+//import android.content.SharedPreferences
+//import android.os.Bundle
+//import android.widget.*
+//import androidx.appcompat.app.AppCompatActivity
+//import org.json.JSONObject
+//
+//class TotalExpenditureActivity : AppCompatActivity() {
+//    private lateinit var radioGroupTimeFrame: RadioGroup
+//    private lateinit var radioButtonDaily: RadioButton
+//    private lateinit var radioButtonWeekly: RadioButton
+//    private lateinit var radioButtonMonthly: RadioButton
+//    private lateinit var textViewTotalExpenditure: TextView
+//
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_total_expenditure)
+//
+//        // Initialize UI elements
+//        radioGroupTimeFrame = findViewById(R.id.radioGroupTimeFrame)
+//        radioButtonDaily = findViewById(R.id.radioButtonDaily)
+//        radioButtonWeekly = findViewById(R.id.radioButtonWeekly)
+//        radioButtonMonthly = findViewById(R.id.radioButtonMonthly)
+//        textViewTotalExpenditure = findViewById(R.id.textViewTotalExpenditure)
+//
+//        // Set up radio button listeners
+//        radioGroupTimeFrame.setOnCheckedChangeListener { group, checkedId ->
+//            when (checkedId) {
+//                R.id.radioButtonDaily -> {
+//                    // Calculate and display total daily expenditure
+//                    val totalDailyExpenditure = calculateTotalExpenditureFor("daily")
+//                    textViewTotalExpenditure.text = "Total Expenditure (Daily): $totalDailyExpenditure"
+//                }
+//                R.id.radioButtonWeekly -> {
+//                    // Calculate and display total weekly expenditure
+//                    val totalWeeklyExpenditure = calculateTotalExpenditureFor("weekly")
+//                    textViewTotalExpenditure.text = "Total Expenditure (Weekly): $totalWeeklyExpenditure"
+//                }
+//                R.id.radioButtonMonthly -> {
+//                    // Calculate and display total monthly expenditure
+//                    val totalMonthlyExpenditure = calculateTotalExpenditureFor("monthly")
+//                    textViewTotalExpenditure.text = "Total Expenditure (Monthly): $totalMonthlyExpenditure"
+//                }
+//            }
+//        }
+//
+//        // Load and display the JSON expense data
+//        val expenseJson = loadExpenseFromSharedPreferences()
+//        displayExpenseData(expenseJson)
+//    }
+//
+//    private fun calculateTotalExpenditureFor(timeFrame: String): Double {
+//        // Placeholder for calculating the total expenditure based on the selected time frame
+//        // You should replace this with your logic to calculate the actual total expenditure
+//        // Sample: Calculate a random total expenditure
+//        return when (timeFrame) {
+//            "daily" -> 75.0
+//            "weekly" -> 450.0
+//            "monthly" -> 1900.0
+//            else -> 0.0
+//        }
+//    }
+//
+//    private fun loadExpenseFromSharedPreferences(): String? {
+//        val sharedPreferences: SharedPreferences =
+//            getSharedPreferences("ExpenseTracker", Context.MODE_PRIVATE)
+//
+//        // Load the JSON expense data from SharedPreferences
+//        return sharedPreferences.getString("expense_data", null)
+//    }
+//
+//    private fun displayExpenseData(expenseJson: String?) {
+//        if (expenseJson != null) {
+//            // Parse the JSON and extract data for display
+//            val jsonObject = JSONObject(expenseJson)
+//            val amount = jsonObject.getString("amount")
+//            val date = jsonObject.getString("date")
+//            val category = jsonObject.getString("category")
+//            val description = jsonObject.getString("description")
+//
+//            // Display the data in a TextView or other UI elements
+//            textViewTotalExpenditure.text = "Amount: $amount\nDate: $date\nCategory: $category\nDescription: $description"
+//        } else {
+//            // Handle the case where there is no expense data to display
+//            textViewTotalExpenditure.text = "No expense data available"
+//        }
+//    }
+//}
+//
